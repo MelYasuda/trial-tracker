9ef9d3b5f9d12a482f94b446f8bfbfedf4aa5144
Meguru Yasuda <meguruyasuda@Megurus-MacBook-Air.local>
2020-11-19 15:06:38 -0800

    accordion calendar works

--- src/components/TrialForm.js
+++ src/components/TrialForm.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
-import {Text, View, StyleSheet } from 'react-native';
+import {Text, View, StyleSheet, Easing, TouchableOpacity } from 'react-native';
 import { Formik, useFormikContext } from 'formik';
 import * as Yup from 'yup';
 import { Input, Button } from 'react-native-elements';
 import CalendarPicker from 'react-native-calendar-picker';
 import moment from 'moment';
+import Accordion from 'react-native-collapsible/Accordion';
+import * as Animatable from 'react-native-animatable';
 
 const DatePickerField = ({ name, onDateChange }) => {
   const formik = useFormikContext();
@@ -32,6 +34,8 @@ export default class TrialForm extends Component {
       super(props);
       this.state = {
         selectedStartDate: null,
+        startingDateActive: [],
+        endingDateActive: [],
       };
     }
     
@@ -44,6 +48,30 @@ export default class TrialForm extends Component {
     dateParse = (date) => {
       return date[1] + ' ' + date[2] + ', ' + date[3];
     }
+
+    _renderHeader = section => {
+      return (
+        <View>
+          <Text style={{color: 'white', fontSize: 30}}>{section.title}</Text>
+        </View>
+      );
+    };
+
+    _renderCalender = section => {
+      return (
+        <View>
+          {section.content}
+        </View>
+      );
+    };
+
+  _openStartingDate = startingDateActive => {
+    this.setState({ startingDateActive: startingDateActive, endingDateActive: [] });
+  };
+
+  _openEndingDate = endingDateActive => {
+    this.setState({ endingDateActive:  endingDateActive, startingDateActive: []});
+  };
     
     render() {
       const { selectedStartDate } = this.state;
@@ -51,33 +79,55 @@ export default class TrialForm extends Component {
       return (
         <View>
           <Formik 
-            initialValues={{ title: '', startDate: moment(), duration: '', endDate: '', note: ''}}
+            initialValues={{ title: '', startDate: moment(), duration: '', endDate: moment(), note: ''}}
             onSubmit={this._handleSubmit}
             validationSchema={Yup.object().shape({
               title: Yup.string().required('Chore description is required'),
               endDate: Yup.string().required('Day needs to be chosen')
             })}
-            component={({
-              values,
-              handleSubmit,
-              setFieldValue,
-              setFieldTouched,
-              errors,
-              touched,
-            }) => (
-              <View>
-                <Text onPress={handleSubmit} style={{color: "orange", textAlign: "right"}}>Save</Text>
-                <Input
-                  placeholder='trial name'
-                  //style={{backgroundColor: 'white'}}
-                />
-                <Text style={{color: "white"}}>Starts {this.dateParse(values.startDate.toString().split(' '))} </Text>
-                <DatePickerField 
-                  name={'startDate'}
-                  onDateChange={this.onDateChange} />
-              </ View>
-            )}
-            />
+          >
+          {props => (
+            <View>
+              <Text onPress={props.handleSubmit} style={{color: "orange", textAlign: "right"}}>Save</Text>
+              <Input
+                placeholder='trial name'
+                //style={{backgroundColor: 'white'}}
+              />
+              <Accordion
+                activeSections={this.state.startingDateActive}
+                renderHeader={this._renderHeader}
+                renderContent={this._renderCalender}
+                onChange={this._openStartingDate}
+                duration={500}
+                touchableComponent={TouchableOpacity}
+                sections={[{
+                  title: this.dateParse(props.values.startDate.toString().split(' ')),
+                  content: <DatePickerField 
+                            name={'startDate'}
+                            onDateChange={this.onDateChange} />
+                },
+              ]}
+              />
+              <Accordion
+                activeSections={this.state.endingDateActive}
+                renderHeader={this._renderHeader}
+                renderContent={this._renderCalender}
+                onChange={this._openEndingDate}
+                duration={500}
+                touchableComponent={TouchableOpacity}
+                sections={[{
+                  title: this.dateParse(props.values.startDate.toString().split(' ')),
+                  content: <DatePickerField 
+                            name={'endDate'}
+                            onDateChange={this.onDateChange} />
+                },
+              ]}
+              />
+              <Text style={{color: 'white'}}>hello</Text>
+            </ View>
+          )}
+
+            </ Formik>
         </View>
         
         
